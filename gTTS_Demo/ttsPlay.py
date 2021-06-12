c7018c3a680e1b4f1ceb31c0482b5b0e19a07de0
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-06-12 16:51:15 +0900

    Update ttsPlay.py

--- gTTS_Demo/ttsPlay.py
+++ gTTS_Demo/ttsPlay.py
@@ -7,21 +7,22 @@ import time
 from gtts import gTTS
 from mutagen.mp3 import MP3
 
-def ttsPlay(message, lang='en', display=True):
+def ttsPlay(message, lang='en', display=True, Speed=1):
     ttsfile = 'ttsplay.mp3'
     media_player = vlc.MediaPlayer()
 
     tts = gTTS(message, lang=lang)
     tts.save(ttsfile)
 
-    media_length = MP3(ttsfile).info.length + 0.1
+    media_length = MP3(ttsfile).info.length + 0.1*Speed
     if display==True:
         print(f"{message} : {media_length:0.2f}sec.")
 
     media = vlc.Media(ttsfile)
     media_player.set_media(media)
+    media_player.set_rate(Speed)
     media_player.play()
-    time.sleep(media_length)
+    time.sleep(media_length/Speed)
 
 if __name__ == "__main__":
     #for i in range(0, 101, 20):
