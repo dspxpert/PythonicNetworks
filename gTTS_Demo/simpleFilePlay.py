45ea91d68dadd7c88ce477636471669f2d7c209e
dspxpert <32154318+dspxpert@users.noreply.github.com>
2021-07-15 11:38:50 +0900

    Delete simpleFilePlay.py

--- gTTS_Demo/simpleFilePlay.py
+++
@@ -1,9 +0,0 @@
-import ttsPlay
-import sys
-
-with open(sys.argv[1]) as fp:
-    for line in fp:
-        if line.strip() == "":
-            continue
-        ttsPlay.ttsPlay(line.strip())
-        
