e764cb4923315ef51b9bec8c6fdbc454e95848ce
dspxpert <32154318+dspxpert@users.noreply.github.com>
2022-10-04 23:33:33 +0900

    Update speech-to-text.py

--- gTTS_Demo/speech-to-text.py
+++ gTTS_Demo/speech-to-text.py
@@ -10,15 +10,15 @@ with sr.Microphone() as source:
     audio = r.listen(source)
 
 # Listen from File(wav, aiff/aiff-c, flac), not mp3
-r = sr.Recognizer()
-with sr.AudioFile('sample.wav') as source:
-    print('Listening...')
-    audio = r.record(source)
+#r = sr.Recognizer()
+#with sr.AudioFile('sample.wav') as source:
+#    print('Listening...')
+#    audio = r.record(source)
 
 try:
     # Using Google API, Limit 50 times/day
-    text = r.recognize_google(audio, language='en-US')
-    print(text)
+    #text = r.recognize_google(audio, language='en-US')
+    #print(text)
     
     # 한글인식
     text = r.recognize_google(audio, language='ko')
